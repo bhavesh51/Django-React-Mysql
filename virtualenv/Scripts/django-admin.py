3756b282bc9ee994109125414e4aa29c966f85ef
Bhavesh Sadhu <bsadhu51@gmail.com>
2020-07-26 03:01:20 +0530

    init setup

---
+++ virtualenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!g:\bhaveshsadhu\code\pythonprojects\django-react-mysql\virtualenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
